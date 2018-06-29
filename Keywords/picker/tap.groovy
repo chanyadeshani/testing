bc70a8615a307fe4b3ea9f1dd7e7c7b6a01b415c
chanyadeshani <chanya.deshani@gmail.com>
2018-06-29 17:27:44 +0900

    deleted a keyword

--- Keywords/picker/tap.groovy
+++ Keywords/picker/tap.groovy
@@ -49,21 +49,6 @@ class tap {
 	 * @param timeout time to wait for element to show up
 	 * @return true if element present, otherwise false
 	 */
-	@Keyword
-	def isElementPresent_Mobile(TestObject to, int timeout){
-		try {
-			KeywordUtil.logInfo("Finding element with id:" + to.getObjectId())
-
-			WebElement element = MobileElementCommonHelper.findElement(to, timeout)
-			if (element != null) {
-				KeywordUtil.markPassed("Object " + to.getObjectId() + " is present")
-			}
-			return true
-		} catch (Exception e) {
-			KeywordUtil.markFailed("Object " + to.getObjectId() + " is not present")
-		}
-		return false;
-	}
 
 	/**
 	 * Get mobile driver for current session
